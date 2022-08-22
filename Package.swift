4d92dbd8c762ff5a3d135733094b6221b82474cd
nk-5 <nkws27@gmail.com>
2022-08-22 18:28:52 +0900

    change lib type

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     products: [
         .library(
             name: "PINRemoteImage",
-            type: .static,
+            //            type: .static,
             targets: ["PINRemoteImage"]),
     ],
     dependencies: [
